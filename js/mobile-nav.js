e8b4120f6f6a84951a9c318357ac8d2867d37e80
Renald <renaldq@gmail.com>
2019-02-10 22:56:27 -0700

    mobile menu

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,14 +1,9 @@
-var menu = document.getElementById('mobile-container')
-var open = document.getElementById('open-mobile')
-var close = document.getElementById('close-mobile')
-
-function openMenu() {
-    menu.className = 'mobile-container open';
-}
-
-function closeMenu() {
-    menu.className = 'mobile-container';
-}
-
-open.addEventListener('click', openMenu);
-close.addEventListener('click', closeMenu);
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  /* Close/hide the sidenav */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
